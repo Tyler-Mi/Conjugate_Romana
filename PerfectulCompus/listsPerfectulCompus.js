912f8a704a7ce76b27b620226529a98c01a83aad
Tyler-Mi <michels.t2024@protonmail.com>
2024-12-11 22:11:18 +0200

    Add files via upload

---
+++ PerfectulCompus/listsPerfectulCompus.js
@@ -0,0 +1,69 @@
+export const listOfVerbs = {
+    "a sta": {
+        "eu": 'am stat',
+        "tu": 'ai stat',
+        "el/ea": 'a stat',
+        "noi": 'am stat',
+        "voi": 'ați stat',
+        "ei/ele": 'au stat'
+    },
+    "a avea": {
+        "eu": 'am avut',
+        "tu": 'ai avut',
+        "el/ea": 'a avut',
+        "noi": 'am avut',
+        "voi": 'ați avut',
+        "ei/ele": 'au avut'
+    },
+    "a ține": {
+        "eu": 'am ținut',
+        "tu": 'ai ținut',
+        "el/ea": 'a ținut',
+        "noi": 'am ținut',
+        "voi": 'ați ținut',
+        "ei/ele": 'au ținut'
+    },
+    "a zice": {
+        "eu": 'am zis',
+        "tu": 'ai zis',
+        "el/ea": 'a zis',
+        "noi": 'am zis',
+        "voi": 'ați zis',
+        "ei/ele": 'au zis'
+    },
+    "a rupe": {
+        "eu": 'am rupt',
+        "tu": 'ai rupt',
+        "el/ea": 'a rupt',
+        "noi": 'am rupt',
+        "voi": 'ați rupt',
+        "ei/ele": 'au rupt'
+    },
+    "a veni": {
+        "eu": 'am venit',
+        "tu": 'ai rupt',
+        "el/ea": 'a rupt',
+        "noi": 'am rupt',
+        "voi": 'ați rupt',
+        "ei/ele": 'au rupt'
+    },
+    "a urî": {
+        "eu": 'am urât',
+        "tu": 'ai urât',
+        "el/ea": 'a urât',
+        "noi": 'am urât',
+        "voi": 'ați urât',
+        "ei/ele": 'au urât'
+    }
+    
+};
+
+
+export const choosePronounList = [
+    'eu',
+    'tu',
+    'el/ea',
+    'noi',
+    'voi',
+    'ei/ele'
+];
