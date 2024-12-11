ea23a4b5ce7242eaf8bdcaf445061ac871a15fd9
Tyler-Mi <michels.t2024@protonmail.com>
2024-12-11 21:15:18 +0200

    Add files via upload

---
+++ ConjugationPractice/PerfectulCompus/listsPerfectulCompus.js
@@ -0,0 +1,21 @@
+export const listOfVerbs = {
+    "a faca": {
+        "eu": 'faca',
+        "tu": 'faca',
+        "el/ea": 'faca',
+        "noi": 'faca',
+        "voi": 'faca',
+        "ei/ele": 'faca'
+    },
+    
+};
+
+
+export const choosePronounList = [
+    'eu',
+    'tu',
+    'el/ea',
+    'noi',
+    'voi',
+    'ei/ele'
+];
