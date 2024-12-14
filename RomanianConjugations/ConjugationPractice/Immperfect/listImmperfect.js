2d5954242e81172fffd36e84e325c309759aa1a2
Tyler-Mi <michels.t2024@protonmail.com>
2024-12-14 20:03:28 +0200

    Add files via upload
    updating the website with some minor fixes:
    -changing the viitoare comun title to say that when in the page
    -correcting the conjugation answers in the viitoare

---
+++ RomanianConjugations/ConjugationPractice/Immperfect/listImmperfect.js
@@ -0,0 +1,126 @@
+export const listOfVerbs = {
+    "a lucra": {
+        "eu": 'lucram',
+        "tu": 'lucrai',
+        "el/ea": 'lucra',
+        "noi": 'lucram',
+        "voi": 'lucrați',
+        "ei/ele": 'lucrau'
+    },
+    "a coborî": {
+        "eu": 'coboram',
+        "tu": 'coborai',
+        "el/ea": 'cobora',
+        "noi": 'coboram',
+        "voi": 'coborați',
+        "ei/ele": 'coborau'
+    },
+    "a putea": {
+        "eu": 'puteam',
+        "tu": 'puteai',
+        "el/ea": 'putea',
+        "noi": 'puteam',
+        "voi": 'puteați',
+        "ei/ele": 'puteau'
+    },
+    "a spune": {
+        "eu": 'spuneam',
+        "tu": 'spuneai',
+        "el/ea": 'spunea',
+        "noi": 'spuneam',
+        "voi": 'spuneați',
+        "ei/ele": 'spuneau'
+    },
+    "a citi": {
+        "eu": 'citeam',
+        "tu": 'citeai',
+        "el/ea": 'citea',
+        "noi": 'citeam',
+        "voi": 'citeați',
+        "ei/ele": 'citeau'
+    },
+    "a face": {
+        "eu": 'făceam',
+        "tu": 'făceai',
+        "el/ea": 'făcea',
+        "noi": 'făceam',
+        "voi": 'făceați',
+        "ei/ele": 'făceau'
+    },
+    "a cunoaște": {
+        "eu": 'cunoșteam',
+        "tu": 'cunoșteai',
+        "el/ea": 'cunoștea',
+        "noi": 'cunoșteam',
+        "voi": 'cunoșteați',
+        "ei/ele": 'cunoșteau'
+    },
+    "a trăi": {
+        "eu": 'trăiam',
+        "tu": 'trăiai',
+        "el/ea": 'trăia',
+        "noi": 'trăiam',
+        "voi": 'trăiați',
+        "ei/ele": 'trăiau'
+    },
+    "a locui": {
+        "eu": 'locuiam',
+        "tu": 'locuiai',
+        "el/ea": 'locuia',
+        "noi": 'locuiam',
+        "voi": 'locuiați',
+        "ei/ele": 'locuiau'
+    },
+    "a scrie": {
+        "eu": 'scriam',
+        "tu": 'scriai',
+        "el/ea": 'scria',
+        "noi": 'scriam',
+        "voi": 'scriați',
+        "ei/ele": 'scriau'
+    },
+    "a ști": {
+        "eu": 'știam',
+        "tu": 'știai',
+        "el/ea": 'știa',
+        "noi": 'știam',
+        "voi": 'știați',
+        "ei/ele": 'știau'
+    },
+    "a sta": {
+        "eu": 'stăteam',
+        "tu": 'stăteai',
+        "el/ea": 'stătea',
+        "noi": 'stăteam',
+        "voi": 'stăteați',
+        "ei/ele": 'stăteau'
+    },
+    "a da": {
+        "eu": 'dădeam',
+        "tu": 'dădeai',
+        "el/ea": 'dădea',
+        "noi": 'dădeam',
+        "voi": 'dădeați',
+        "ei/ele": 'dădeau'
+    },
+    "a fi": {
+        "eu": 'eram',
+        "tu": 'erai',
+        "el/ea": 'era',
+        "noi": 'eram',
+        "voi": 'erați',
+        "ei/ele": 'erau'
+    },
+    },
+    
+};
+
+
+export const choosePronounList = [
+    'eu',
+    'tu',
+    'el/ea',
+    'noi',
+    'voi',
+    'ei/ele'
+];
---
+++ RomanianConjugations/ConjugationPractice/PresentTense/listsPresent.js
@@ -0,0 +1,326 @@
+export const listOfVerbs = {
+    "a face": {
+        "eu": 'fac',
+        "tu": 'faci',
+        "el/ea": 'face',
+        "noi": 'facem',
+        "voi": 'faceți',
+        "ei/ele": 'fac'
+    },
+    "a cânta": {
+        "eu": 'cânt',
+        "tu": 'cânți',
+        "el/ea": 'cântă',
+        "noi": 'cântăm',
+        "voi": 'cântați',
+        "ei/ele": 'cântă'
+    },
+    "a comanda": {
+        "eu": "comand",
+        "tu": "comanzi",
+        "el/ea": "comandă",
+        "noi": "comandăm",
+        "voi": "comandați",
+        "ei/ele": "comandă"
+    },
+    "a lăsa": {
+        "eu": "las",
+        "tu": "lași",
+        "el/ea": "lasă",
+        "noi": "lăsăm",
+        "voi": "lăsați",
+        "ei/ele": "lasă"
+    },
+    "a gusta": {
+        "eu": "gust",
+        "tu": "guști",
+        "el/ea": "gustă",
+        "noi": "gustăm",
+        "voi": "gustați",
+        "ei/ele": "gustă"
+    },
+    "a risca": {
+        "eu": "risc",
+        "tu": "riști",
+        "el/ea": "riscă",
+        "noi": "riscăm",
+        "voi": "riscați",
+        "ei/ele": "riscă"
+    },
+    "a mușca": {
+        "eu": "mușc",
+        "tu": "muști",
+        "el/ea": "mușcă",
+        "noi": "mușcăm",
+        "voi": "mușcați",
+        "ei/ele": "mușcă"
+    },
+    "a alerga": {
+        "eu": "alerg",
+        "tu": "alergi",
+        "el/ea": "aleargă",
+        "noi": "alergăm",
+        "voi": "alergați",
+        "ei/ele": "aleargă"
+    },
+    "a așeza": {
+        "eu": "așez",
+        "tu": "așezi",
+        "el/ea": "așază",
+        "noi": "așezăm",
+        "voi": "așezăți",
+        "ei/ele": "așază"
+    },
+    "a agăța": {
+        "eu": "agăț",
+        "tu": "agăți",
+        "el/ea": "agață",
+        "noi": "agățăm",
+        "voi": "agățați",
+        "ei/ele": "agață"
+    },
+    "a juca": {
+        "eu": "joc",
+        "tu": "joci",
+        "el/ea": "joacă",
+        "noi": "jucăm",
+        "voi": "jucăți",
+        "ei/ele": "joacă"
+    },
+    "a învăța": {
+        "eu": "învăț",
+        "tu": "înveți",
+        "el/ea": "învață",
+        "noi": "învățăm",
+        "voi": "învățați",
+        "ei/ele": "învață"
+    },
+    "a cumpăra": {
+        "eu": "cumpăr",
+        "tu": "cumperi",
+        "el/ea": "cumpără",
+        "noi": "cumpărăm",
+        "voi": "cumpărați",
+        "ei/ele": "cumpără"
+    },
+    "a lucra": {
+        "eu": "lucrez",
+        "tu": "lucrezi",
+        "el/ea": "lucrează",
+        "noi": "lucrăm",
+        "voi": "lucrați",
+        "ei/ele": "lucrează"
+    },
+    "a părea": {
+        "eu": "par",
+        "tu": "pari",
+        "el/ea": "pare",
+        "noi": "părem",
+        "voi": "păreți",
+        "ei/ele": "par"
+    },
+    "a putea": {
+        "eu": "pot",
+        "tu": "poți",
+        "el/ea": "poate",
+        "noi": "putem",
+        "voi": "puteți",
+        "ei/ele": "pot"
+    },
+    "a cădea": {
+        "eu": "cad",
+        "tu": "cazi",
+        "el/ea": "cade",
+        "noi": "cădem",
+        "voi": "cădeți",
+        "ei/ele": "cad"
+    },
+    "a vedea": {
+        "eu": "văd",
+        "tu": "vezi",
+        "el/ea": "vede",
+        "noi": "vedem",
+        "voi": "vedeți",
+        "ei/ele": "văd"
+    },
+    "a tăcea": {
+        "eu": "tac",
+        "tu": "taci",
+        "el/ea": "tace",
+        "noi": "tăcem",
+        "voi": "tăceți",
+        "ei/ele": "tac"
+    },
+    "a promite": {
+        "eu": "promit",
+        "tu": "promiți",
+        "el/ea": "promite",
+        "noi": "promitem",
+        "voi": "promiteți",
+        "ei/ele": "promit"
+    },
+    "a răspunde": {
+        "eu": "răspund",
+        "tu": "răspunzi",
+        "el/ea": "răspunde",
+        "noi": "răspundem",
+        "voi": "răspundeți",
+        "ei/ele": "răspund"
+    },
+    "a coase": {
+        "eu": "cos",
+        "tu": "coși",
+        "el/ea": "coase",
+        "noi": "coasem",
+        "voi": "coaseți",
+        "ei/ele": "cos"
+    },
+    "a crește": {
+        "eu": "cresc",
+        "tu": "crești",
+        "el/ea": "crește",
+        "noi": "creștem",
+        "voi": "creșteți",
+        "ei/ele": "cresc"
+    },
+    "a coace": {
+        "eu": "coc",
+        "tu": "coci",
+        "el/ea": "coace",
+        "noi": "coacem",
+        "voi": "coaceți",
+        "ei/ele": "coc"
+    },
+    "a fugi": {
+        "eu": "fug",
+        "tu": "fugi",
+        "el/ea": "fuge",
+        "noi": "fugim",
+        "voi": "fugiți",
+        "ei/ele": "fug"
+    },
+    "a minți": {
+        "eu": "mint",
+        "tu": "minți",
+        "el/ea": "minte",
+        "noi": "mințim",
+        "voi": "mințiți",
+        "ei/ele": "mint"
+    },
+    "a auzi": {
+        "eu": "aud",
+        "tu": "auzi",
+        "el/ea": "aude",
+        "noi": "auzim",
+        "voi": "auziți",
+        "ei/ele": "aud"
+    },
+    "a ieși": {
+        "eu": "ies",
+        "tu": "ieși",
+        "el/ea": "iese",
+        "noi": "ieșim",
+        "voi": "ieșiți",
+        "ei/ele": "ies"
+    },
+    "a mirosi": {
+        "eu": "miros",
+        "tu": "miroși",
+        "el/ea": "miroase",
+        "noi": "mirosim",
+        "voi": "mirosiți",
+        "ei/ele": "miros"
+    },
+    "a sări": {
+        "eu": "sar",
+        "tu": "sari",
+        "el/ea": "sare",
+        "noi": "sărim",
+        "voi": "săriți",
+        "ei/ele": "sar"
+    },
+    "a veni": {
+        "eu": "vin",
+        "tu": "vii",
+        "el/ea": "vine",
+        "noi": "venim",
+        "voi": "veniți",
+        "ei/ele": "vin"
+    },
+    "a suferi": {
+        "eu": "sufăr",
+        "tu": "suferi",
+        "el/ea": "suferă",
+        "noi": "suferim",
+        "voi": "suferiți",
+        "ei/ele": "suferă"
+    },
+    "a dormi": {
+        "eu": "dorm",
+        "tu": "dormi",
+        "el/ea": "doarme",
+        "noi": "dormim",
+        "voi": "dormiți",
+        "ei/ele": "dorm"
+    },
+    "a muri": {
+        "eu": "mor",
+        "tu": "mori",
+        "el/ea": "moare",
+        "noi": "murim",
+        "voi": "muriți",
+        "ei/ele": "mor"
+    },
+    "a citi": {
+        "eu": "citesc",
+        "tu": "citești",
+        "el/ea": "citește",
+        "noi": "citim",
+        "voi": "citiți",
+        "ei/ele": "citesc"
+    },
+    "a vârî": {
+        "eu": "vâr",
+        "tu": "vâri",
+        "el/ea": "vâră",
+        "noi": "vârâm",
+        "voi": "vârâți",
+        "ei/ele": "vâră"
+    },
+    "a coborî": {
+        "eu": "cobor",
+        "tu": "cobori",
+        "el/ea": "coboară",
+        "noi": "coborâm",
+        "voi": "coborâți",
+        "ei/ele": "coboară"
+    },
+    "a tăbărî": {
+        "eu": "tabăr",
+        "tu": "taberi",
+        "el/ea": "tabără",
+        "noi": "tăbărâm",
+        "voi": "tăbărâți",
+        "ei/ele": "tabără"
+    },
+    "a urî": {
+        "eu": "urăsc",
+        "tu": "urăști",
+        "el/ea": "urăște",
+        "noi": "urâm",
+        "voi": "urâți",
+        "ei/ele": "urăsc"
+    },
+
+    
+};
+
+
+export const choosePronounList = [
+    'eu',
+    'tu',
+    'el/ea',
+    'noi',
+    'voi',
+    'ei/ele'
+];
