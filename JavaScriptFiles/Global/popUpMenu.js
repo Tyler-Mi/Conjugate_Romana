a978e99042aa05000394b0deb15cc1fd2372f854
Tyler-Mi <michels.t2024@protonmail.com>
2025-02-12 19:58:43 +0200

    Add files via upload
    adding the correct files to create the blog and add the extra present tense extras

---
+++ JavaScriptFiles/Global/popUpMenu.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = document.querySelector('.content');
+        if (content.style.display === "flex") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "flex"; // Ensure it uses flex layout
+        }
+      });
+    }
+  });
+  
